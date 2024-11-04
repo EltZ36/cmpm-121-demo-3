1222b2758a1369265ce12faa42a5de93ab595019
Elton Zeng <poptako9@gmail.com>
2024-11-04 09:48:51 -0800

    Main/Added in button for the L16 Dialog.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 // todo
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const alertButton = document.createElement("button");
+alertButton.id = "alertButton";
+alertButton.innerHTML = "Alert";
+alertButton.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+app.append(alertButton);
